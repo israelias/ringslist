15a833a13303e876fbf1294cf2db489a9d2ed15d
israelias <israelias.js@gmail.com>
2021-06-08 23:52:40 +0800

    Add bluprint registration to app.py prior to destructuring

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,88 @@
 import os
 
-from flask import Flask, jsonify, request
-from flask_marshmallow import Marshmallow
-from flask_sqlalchemy import SQLAlchemy
+from flask import Blueprint, Flask, jsonify
+from flask_bcrypt import Bcrypt
+from flask_cors import CORS
+from flask_jwt_extended import JWTManager
+from flask_restplus import Api
+from marshmallow import ValidationError
 
+from database.db import db
+from database.ma import ma
+from resources.auth import SignInApi, SignOutApi, SignUpApi, user_ns
+from resources.category import CategoriesAPI, CategoryAPI, categories_ns, category_ns
+from resources.listing import ListingAPI, ListingsAPI, listing_ns, listings_ns
+
+# from werkzeug.utils import cached_property
+
+# from database.db import initialize_db
+# from database.ma import initialize_ma
+
+
+if not os.path.exists("env.py"):
+    pass
+else:
+    import env
+
+# init app
 app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# bluprint
+bluePrint = Blueprint("api", __name__, url_prefix="/api")
+api = Api(bluePrint, doc="/doc", title="Sample Flask-RestPlus Application")
+app.register_blueprint(bluePrint)
+
+# database configs
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "db.sqlite")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["PROPAGATE_EXCEPTIONS"] = True
+
+# jwt
+app.config["JWT_SECRET_KEY"] = os.environ.get("JWT_SECRET_KEY")
+jwt = JWTManager(app)
+
+# bcrypt
+bcrypt = Bcrypt(app)
+
+# Cors
+CORS(app, supports_credentials=True)
+app.secret_key = os.environ.get("SECRET_KEY")
+app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
+
+
+# init db
+# db = SQLAlchemy(app)
+# initialize_db(app)
+
+# init ma
+# ma = Marshmallow(app)
+# initialize_ma(app)
+
+api.add_namespace(listing_ns)
+api.add_namespace(listings_ns)
+api.add_namespace(category_ns)
+api.add_namespace(categories_ns)
+api.add_namespace(user_ns)
+
+
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+
+@api.errorhandler(ValidationError)
+def handle_validation_error(error):
+    return jsonify(error.messages), 400
+
+
+listing_ns.add_resource(ListingAPI, "/<int:id>")
+listings_ns.add_resource(ListingsAPI, "")
+category_ns.add_resource(CategoryAPI, "/<int:id>")
+categories_ns.add_resource(CategoriesAPI, "")
 
 
 if __name__ == "__main__":
+    db.init_app(app)
+    ma.init_app(app)
     app.run(debug=True)
