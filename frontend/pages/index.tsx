323bf1435046f7b45b7f52386caaa5d859312b8b
israelias <israelias.js@gmail.com>
2021-06-09 01:45:47 +0800

    Write getStaticProps to load initial data for home.tsx
    Additionally, place material-ui draft 'listings' view following card-content boilerplate

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,15 +1,75 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Box from '@material-ui/core/Box';
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Link from 'next/link';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 
-export default IndexPage
+import { useStyles } from '../styles/listings.styles';
+import Layout from '../components/Layout';
+
+import { Listing, Category } from '../interfaces';
+
+type Props = {
+  listings: Listing[];
+  categories: Category[];
+};
+
+const Home = ({ listings, categories }: Props) => {
+  const classes = useStyles();
+  return (
+    <Layout
+      title="RingsListings: All Listings"
+      categories={categories}
+    >
+      <main>
+        <Container className={classes.cardGrid} maxWidth="lg">
+          <Grid container spacing={2}>
+            {listings.map((listing) => (
+              <Link
+                key={listing.id}
+                href={`listing/${encodeURIComponent(listing.slug)}`}
+              >
+                <Grid item xs={6} sm={4} md={3}>
+                  <Card className={classes.card} elevation={0}>
+                    <CardContent>
+                      <Typography gutterBottom component="p">
+                        {listing.title}, from {listing.user}
+                      </Typography>
+                      <Box
+                        component="p"
+                        fontSize={16}
+                        fontWeight={900}
+                      >
+                        {listing.description}
+                      </Box>
+                    </CardContent>
+                  </Card>
+                </Grid>
+              </Link>
+            ))}
+          </Grid>
+        </Container>
+      </main>
+    </Layout>
+  );
+};
+
+export async function getStaticProps() {
+  const res = await fetch('http://localhost:5000/api/listings');
+  const listings = await res.json();
+
+  const ress = await fetch('http://localhost:5000/api/categories');
+  const categories = await ress.json();
+
+  return {
+    props: {
+      listings,
+      categories,
+    },
+  };
+}
+
+export default Home;
