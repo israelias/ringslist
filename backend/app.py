6803e3fd35486f98d9175888eb25001cd442ff0a
israelias <israelias.js@gmail.com>
2021-06-09 18:02:42 +0800

    Separate development and production apps

--- backend/app.py
+++ backend/app.py
@@ -7,93 +7,78 @@ from flask_jwt_extended import JWTManager
 from flask_restplus import Api
 from marshmallow import ValidationError
 
-from database.db import db
-from database.ma import ma
-from resources.auth import (
-    SignInApi,
-    SignOutApi,
-    SignUpApi,
-    signin_ns,
-    signout_ns,
-    signup_ns,
-)
-from resources.category import CategoriesAPI, CategoryAPI, categories_ns, category_ns
-from resources.listing import ListingAPI, ListingsAPI, listing_ns, listings_ns
-from resources.user import UserAPI, UsersAPI, user_ns, users_ns
-
+from database.db import db, initialize_db
+from database.ma import initialize_ma, ma
 
 if not os.path.exists("env.py"):
     pass
 else:
     import env
 
-# init app
+# ===========================================================================
+# *               `Flask App and Configs`
+# ? Executes Flask app deployment
+# Initializes app and packages by order of dependency requirements
+# ===========================================================================
+
+
+# Initialize `app`
 app = Flask(__name__)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-# bluprint
+# Import modules that require `app`
+from core.routes import initialize_routes
+
+# Initialize `Blueprint API`
 bluePrint = Blueprint("api", __name__, url_prefix="/api")
-api = Api(bluePrint, doc="/doc", title="Sample Flask-RestPlus Application")
+api = Api(bluePrint, doc="/doc", title="Ring's Listings Restful Backend API")
 app.register_blueprint(bluePrint)
 
-# database configs
+# Import modules that require 'api'
+from core.namespace import initialize_namespaces
+
+# Apply database configs
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "db.sqlite")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["PROPAGATE_EXCEPTIONS"] = True
 
-# jwt
+# Launch JWT manager `jwt`
 app.config["JWT_SECRET_KEY"] = os.environ.get("JWT_SECRET_KEY")
 jwt = JWTManager(app)
 
-# bcrypt
+# Initialize `bcrypt`
 bcrypt = Bcrypt(app)
 
-# Cors
+# Initialize `CORS` and cors configs
 CORS(app, supports_credentials=True)
 app.secret_key = os.environ.get("SECRET_KEY")
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 
+# Launch Database
+initialize_db(app)
 
-# init db
-# db = SQLAlchemy(app)
-# initialize_db(app)
+# Launch Marshmallow serializer
+initialize_ma(app)
 
-# init ma
-# ma = Marshmallow(app)
-# initialize_ma(app)
+# Launch Swagger-ui namespaces
+initialize_namespaces()
 
-api.add_namespace(listing_ns)
-api.add_namespace(listings_ns)
-api.add_namespace(category_ns)
-api.add_namespace(categories_ns)
-api.add_namespace(signup_ns)
-api.add_namespace(signin_ns)
-api.add_namespace(signout_ns)
-api.add_namespace(users_ns)
-api.add_namespace(user_ns)
-
-
+# Initialize database table base
 @app.before_first_request
 def create_tables():
     db.create_all()
 
 
+# Initialize error handler
 @api.errorhandler(ValidationError)
 def handle_validation_error(error):
     return jsonify(error.messages), 400
 
 
-listing_ns.add_resource(ListingAPI, "/<int:id>")
-listings_ns.add_resource(ListingsAPI, "")
-category_ns.add_resource(CategoryAPI, "/<int:id>")
-categories_ns.add_resource(CategoriesAPI, "")
-signup_ns.add_resource(SignUpApi, "")
-signin_ns.add_resource(SignInApi, "")
-signout_ns.add_resource(SignOutApi, "")
-user_ns.add_resource(UserAPI, "/<int:id>")
-users_ns.add_resource(UsersAPI, "")
+# Launch routes
+initialize_routes()
 
+
+# Heroku `run` init.
 if __name__ == "__main__":
-    db.init_app(app)
-    ma.init_app(app)
-    app.run(debug=True)
+    app.run()
