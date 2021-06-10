af48079484f75361873956c5694c60fc04934668
israelias <israelias.js@gmail.com>
2021-06-10 12:44:43 +0800

    Wrap app with context API for site-wide data availability

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -3,6 +3,8 @@ import { AppProps } from 'next/app';
 import { ThemeProvider } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import theme from '../styles/theme';
+import { UserProvider } from '../context/user.context';
+import { DataHandlerProvider } from '../context/data.context';
 import '../styles/globals.css';
 
 export default function MyApp(props: AppProps) {
@@ -19,9 +21,13 @@ export default function MyApp(props: AppProps) {
   return (
     <React.Fragment>
       <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Component {...pageProps} />
+        <UserProvider>
+          <DataHandlerProvider>
+            {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+            <CssBaseline />
+            <Component {...pageProps} />
+          </DataHandlerProvider>
+        </UserProvider>
       </ThemeProvider>
     </React.Fragment>
   );
