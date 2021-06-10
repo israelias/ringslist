ebd62c1e797fe8603d123244424f99584af35a40
israelias <israelias.js@gmail.com>
2021-06-10 12:47:34 +0800

    Merge copyright data to a layout-specific footer

---
+++ frontend/components/Footer.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import Typography from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {'Copyright © '}
+      <Link
+        color="inherit"
+        href="https://github.com/israelias/ringslist"
+      >
+        RL / Joem Elias Sanez @israelias
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
+
+const useStyles = makeStyles((theme) => ({
+  footer: {
+    backgroundColor: theme.palette.background.paper,
+    // marginTop: theme.spacing(8),
+    padding: theme.spacing(6, 0),
+  },
+}));
+
+export default function Footer({
+  description,
+  title,
+}: {
+  description: string;
+  title: string;
+}) {
+  const classes = useStyles();
+
+  return (
+    <footer className={classes.footer}>
+      <Container maxWidth="lg">
+        <Typography variant="h6" align="center" gutterBottom>
+          {title}
+        </Typography>
+        <Typography
+          variant="subtitle1"
+          align="center"
+          color="textSecondary"
+          component="p"
+        >
+          {description}
+        </Typography>
+        <Copyright />
+      </Container>
+    </footer>
+  );
+}
