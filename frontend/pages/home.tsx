5122d2c1f943d6cb5dcbcae5ed5a5bd98816685e
israelias <israelias.js@gmail.com>
2021-06-10 12:52:40 +0800

    Move listings from index to home page

---
+++ frontend/pages/home.tsx
@@ -0,0 +1,44 @@
+import Feed from '../components/Feed';
+import Layout from '../components/Layout';
+import ListingCard from '../components/Card';
+import { Listing, Category } from '../interfaces';
+
+type Props = {
+  listings: Listing[];
+  categories: Category[];
+};
+
+const Home = ({ listings, categories }: Props) => {
+  return (
+    <Layout
+      title="RingsListings: All Listings"
+      categories={categories}
+    >
+      <main>
+        {/* <Feed listings={listings} /> */}
+        <ListingCard listings={listings} />
+      </main>
+    </Layout>
+  );
+};
+
+export async function getStaticProps() {
+  const res = await fetch(
+    'https://rlist-backend.herokuapp.com/api/listings'
+  );
+  const listings = await res.json();
+
+  const ress = await fetch(
+    'https://rlist-backend.herokuapp.com/api/categories'
+  );
+  const categories = await ress.json();
+
+  return {
+    props: {
+      listings,
+      categories,
+    },
+  };
+}
+
+export default Home;
