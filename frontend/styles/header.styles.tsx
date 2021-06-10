bde7e710a21464ef8c924fdb7862c86d96ca73aa
israelias <israelias.js@gmail.com>
2021-06-10 13:40:31 +0800

    Fix typescript theme object

--- frontend/styles/header.styles.tsx
+++ frontend/styles/header.styles.tsx
@@ -27,7 +27,7 @@ export const useStyles = makeStyles((theme) => ({
     padding: '0px',
     minHeight: 60,
     display: 'flex',
-
+    paddingTop: theme.spacing(0),
     justifyContent: 'space-between',
   },
   toolbarSecondary: {
