2ac8880023f5c5f490ab58fc5a92f3f20c82e083
israelias <israelias.js@gmail.com>
2021-06-10 12:49:29 +0800

    Apply footer and navbar conditionals to master layout

--- frontend/components/Layout.tsx
+++ frontend/components/Layout.tsx
@@ -1,22 +1,27 @@
 import React, { ReactNode } from 'react';
 
 import Head from 'next/head';
+import { CssBaseline } from '@material-ui/core';
 
-import Header from './Header';
+import Footer from './Footer';
+import Navbar from './Navbar';
 import { Category } from '../interfaces';
 
 type Props = {
   children?: ReactNode;
   title?: string;
   categories: Category[];
+  about?: string;
 };
 
 const Layout = ({
   children,
   title = "Ring's Listings",
+  about = 'Meaningful Classifieds',
   categories,
 }: Props) => (
-  <div>
+  <>
+    <CssBaseline />
     <Head>
       <title>{title}</title>
       <meta charSet="utf-8" />
@@ -25,15 +30,14 @@ const Layout = ({
         content="initial-scale=1.0, width=device-width"
       />
     </Head>
-    <header>
-      <Header categories={categories} />
-    </header>
-    {children}
-    <footer>
-      <hr />
-      <span>I'm here to stay (Footer)</span>
-    </footer>
-  </div>
+    <>
+      <>
+        <Navbar categories={categories} />
+      </>
+      {children}
+      <Footer title={title} description={about} />
+    </>
+  </>
 );
 
 export default Layout;
